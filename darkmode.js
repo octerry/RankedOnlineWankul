615fcc9ceb6eb409dd87964b7b1567404ce8b77e
Octerry <taaniyu@gmail.com>
2025-12-17 19:31:00 +0100

    Fin du darkmode, début de la refonte graphite par rapport au logo

--- darkmode.js
+++ darkmode.js
@@ -1,8 +1,22 @@
 //On récupère tous les elements de la page
 let col = document.querySelectorAll('*')
+let darkmodeButton = document.getElementById('darkmodeButton')
+let isDarkmodeActivate = true
 
-console.log(col)
+// for (const element of col) {
+//     element.classList.add("light")
+// }
 
-for (const element of col) {
-    element.classList.add("light")
-}
+darkmodeButton.addEventListener('click', function () {
+    if (isDarkmodeActivate) {
+        for (const element of col) {
+            element.classList.add("light")
+            isDarkmodeActivate = false
+        }
+    } else {
+        for (const element of col) {
+            element.classList.remove("light")
+            isDarkmodeActivate = true
+        }
+    }
+})
