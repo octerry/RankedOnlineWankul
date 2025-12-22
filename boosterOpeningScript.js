0a98106b732e21feba6b7b912c979b2f94a80fbf
Octerry <taaniyu@gmail.com>
2025-12-22 14:12:00 +0100

    Petit changement de js pour scale les boosters

--- boosterOpeningScript.js
+++ boosterOpeningScript.js
@@ -17,20 +17,33 @@ boosterOpening.addEventListener('scroll', function () {
     console.log(boosterOpening.scrollTop/window.innerHeight)
     if ((boosterOpening.scrollTop/window.innerHeight) < .4) {
         seasonTitle.innerHTML = "Saison Aléatoire"
+        boosters[0].style.transform = ""
+        boosters[1].style.transform = "scale(.8)"
         boosterBackground.style.backgroundColor = 'currentColor'
     } else {
         if ((boosterOpening.scrollTop/window.innerHeight) < .9) {
+            boosters[0].style.transform = "scale(.8)"    
+            boosters[1].style.transform = ""  
+            boosters[2].style.transform = "scale(.8)"    
             seasonTitle.innerHTML = "Saison 1 ORIGINS"
             boosterBackground.style.backgroundColor = '#183272'
         } else {
             if ((boosterOpening.scrollTop/window.innerHeight) < 1.4) {
+                boosters[1].style.transform = "scale(.8)"    
+                boosters[2].style.transform = ""  
+                boosters[3].style.transform = "scale(.8)"    
                 seasonTitle.innerHTML = "Saison 2 CAMPUS"
                 boosterBackground.style.backgroundColor = '#2e6428ff'
             } else {
                 if ((boosterOpening.scrollTop/window.innerHeight) < 1.8) {
+                    boosters[2].style.transform = "scale(.8)"    
+                    boosters[3].style.transform = ""  
+                    boosters[4].style.transform = "scale(.8)"    
                     seasonTitle.innerHTML = "Saison 3 BATTLE"
                     boosterBackground.style.backgroundColor = '#791e1e'
                 } else {
+                    boosters[3].style.transform = "scale(.8)"    
+                    boosters[4].style.transform = ""  
                     seasonTitle.innerHTML = "Saison 4 STELLAR"
                     boosterBackground.style.backgroundColor = '#4c2579ff'
                 }
