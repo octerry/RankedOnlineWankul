4e8a7edc8c99f404ab7fd51a4a82fdc491953beb
Octerry <taaniyu@gmail.com>
2026-04-04 16:11:52 +0200

    fix: url bug

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let userID = null;
 if (localStorage.getItem('userID')) {
     userID = JSON.parse(localStorage.getItem('userID'))
 } else {
-    location.href = "/connexion"
+    location.href = "/connexion/"
 }
 
 fetch("https://vps.terrysegaunes.fr/row-backend/src/getAPI.php") // Adresse de mon VPS personnel :)
