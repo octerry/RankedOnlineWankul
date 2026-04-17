4c1ce086603908d43de5d816bb46c0c760dc1888
Octerry <taaniyu@gmail.com>
2026-04-17 21:31:07 +0200

    Je galère avec PHP

---
+++ Backend/src/sigin.php
@@ -0,0 +1,26 @@
+<?php
+
+try {
+    require "connection.php";
+
+    $name = $_POST["name"];
+    $password = $_POST["password"];
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    $result = $pdo->query("SELECT name FROM login");
+    $names = $result->fetch(PDO::FETCH_ASSOC);
+
+    echo $names["name"];
+
+    $stmt = $pdo->prepare("INSERT INTO login (name, password) VALUES (:name, :password)");
+    $stmt->execute([
+        "name" => $name,
+        "password"=> $password
+    ]);
+
+    echo "Bien enregistré :)"
+} catch (Exception $e) {
+    echo "Erreur : ". $e->getMessage() ."";
+}
+
+?>
